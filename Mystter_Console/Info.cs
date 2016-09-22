1ce33806355d7f2c391758199c58a1dbb33bf518
Mystasly <mystasly48@gmail.com>
2016-09-22 16:57:38 +0900

    Deleted many of using

--- Mystter_Console/Info.cs
+++ Mystter_Console/Info.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Mystter_Console {
+﻿namespace Mystter_Console {
     public class Info {
         public static readonly string Name = "Mystter - Console";
     }
