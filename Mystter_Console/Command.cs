cdbad0d69ddc0f3b154d916c0f5b66d5f7bcf1d9
Mystasly <mystasly48@gmail.com>
2016-09-23 22:34:12 +0900

    Added switch commands

--- Mystter_Console/Command.cs
+++ Mystter_Console/Command.cs
@@ -1,117 +1,70 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Mystter_Console {
     public class Command {
-        public static void Read() {
+        public static string ReadLine() {
             Console.Write(">> ");
-            SwitchCommands(GetCommand(Console.ReadLine()));
+            return Console.ReadLine();
         }
 
-        private static Commands GetCommand(string str) {
-            if (str.Contains("help")) return Commands.Help;
-            if (str.Contains("exit")) return Commands.Exit;
-            if (str.Contains("clear")) return Commands.Clear;
-            if (str.Contains("current ")) return Commands.Current;
-            if (str.Contains("list ")) return Commands.List;
-            if (str.Contains("add ")) return Commands.Add;
-            if (str.Contains("switch ")) return Commands.Switch;
-            if (str.Contains("tweet ")) return Commands.Tweet;
-            if (str.Contains("tweets ")) return Commands.Tweets;
-            if (str.Contains("delete ")) return Commands.Delete;
-            if (str.Contains("remove ")) return Commands.Remove;
-            if (str.Contains("media ")) return Commands.Media;
-            return Commands.Unknown;
+        public static void ReadCommand() {
+            SwitchCommands(ReadLine());
         }
 
-        private static void SwitchCommands(Commands cmd) {
-            switch (cmd) {
-                case Commands.Help:
-                    ShowHelp();
-                    break;
+        private static void SwitchCommands(string str) {
+            if (str.Contains(Commands.Help)) {
+                Console.WriteLine("help - ヘルプを表示");
+                Console.WriteLine("exit - プログラムを終了");
+                Console.WriteLine("clear - コンソールを初期化");
+                Console.WriteLine("current - ログイン中のアカウントを表示");
+                Console.WriteLine("list - 登録されているアカウントを表示 *未実装*");
+                Console.WriteLine("add [名前] - アカウントを登録");
+                Console.WriteLine("switch [名前] - アカウントを切り替え");
+                Console.WriteLine("tweet [文字列] - 文字列をツイート");
+                Console.WriteLine("tweets [数値] - 過去ツイートを[数値]件表示（最大５０件） *未実装*");
+                Console.WriteLine("delete [数値] - [数値]前のツイートを削除（最大２００件前） *未実装*");
+                Console.WriteLine("remove [名前] - 登録されているアカウント[名前]を削除 *未実装*");
+                Console.WriteLine("image [文字列] [場所] - 画像[場所]を添付して[文字列]をツイート *未実装*");
+            } else if (str.Contains(Commands.Exit)) {
+                return;
+            } else if (str.Contains(Commands.Clear)) {
+                Console.Clear();
+            } else if (str.Contains(Commands.Current)) {
+                var param = ExtractParam(str, Commands.Current);
 
-                case Commands.Exit:
-                    return;
+            } else if (str.Contains(Commands.List)) {
+                var param = ExtractParam(str, Commands.List);
 
-                case Commands.Clear:
-                    Console.Clear();
-                    break;
+            } else if (str.Contains(Commands.Add)) {
+                var param = ExtractParam(str, Commands.Add);
+                Twitter.AddAccount(param);
+            } else if (str.Contains(Commands.Switch)) {
+                var param = ExtractParam(str, Commands.Switch);
 
-                case Commands.Current:
-
-                    break;
-
-                case Commands.List:
-
-                    break;
-
-                case Commands.Add:
-
-                    break;
-
-                case Commands.Switch:
-
-                    break;
-
-                case Commands.Tweet:
-
-                    break;
-
-                case Commands.Tweets:
-
-                    break;
-
-                case Commands.Delete:
-
-                    break;
-
-                case Commands.Remove:
-
-                    break;
-
-                case Commands.Media:
-
-                    break;
-
-                case Commands.Unknown:
-                    Console.WriteLine("不明なコマンドです。 help でコマンドを確認することができます。");
-                    break;
+            } else if (str.Contains(Commands.Tweet)) {
+                var param = ExtractParam(str, Commands.Tweet);
+                Twitter.SendTweet(param);
+            } else if (str.Contains(Commands.Tweets)) {
+                var param = ExtractParam(str, Commands.Tweets);
+                Console.WriteLine("申し訳ありませんが、このコマンドは現在実装中です。");
+            } else if (str.Contains(Commands.Delete)) {
+                var param = ExtractParam(str, Commands.Delete);
+                Console.WriteLine("申し訳ありませんが、このコマンドは現在実装中です。");
+            } else if (str.Contains(Commands.Remove)) {
+                var param = ExtractParam(str, Commands.Remove);
+                Console.WriteLine("申し訳ありませんが、このコマンドは現在実装中です。");
+            } else if (str.Contains(Commands.Image)) {
+                var param = ExtractParam(str, Commands.Image);
+                Console.WriteLine("申し訳ありませんが、このコマンドは現在実装中です。");
+            } else {
+                Console.WriteLine("不明なコマンドです。 help でコマンドを確認することができます。");
             }
-            Read();
+            ReadCommand();
         }
 
-        private enum Commands {
-            Unknown,
-            Help,
-            Exit,
-            Clear,
-            Current,
-            List,
-            Add,
-            Switch,
-            Tweet,
-            Tweets,
-            Delete,
-            Remove,
-            Media
-        }
-
-        private static void ShowHelp() {
-            Console.WriteLine("help - ヘルプを表示");
-            Console.WriteLine("exit - プログラムを終了");
-            Console.WriteLine("clear - コンソールを初期化");
-            Console.WriteLine("current - ログイン中のアカウントを表示");
-            Console.WriteLine("list - 登録されているアカウントを表示 *未実装*");
-            Console.WriteLine("add [名前] - アカウントを登録");
-            Console.WriteLine("switch [名前] - アカウントを切り替え");
-            Console.WriteLine("tweet [文字列] - 文字列をツイート");
-            Console.WriteLine("tweets [数値] - 過去ツイートを[数値]件表示（最大５０件） *未実装*");
-            Console.WriteLine("delete [数値] - [数値]前のツイートを削除（最大２００件前） *未実装*");
-            Console.WriteLine("remove [名前] - 登録されているアカウント[名前]を削除 *未実装*");
-            Console.WriteLine("media [文字列] [場所] - メディア[場所]を添付して[文字列]をツイート *未実装*");
+        private static string ExtractParam(string text, string cmd) {
+            var param = text.Substring(cmd.Length); 
+            return param;
         }
     }
 }
